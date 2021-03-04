8748ddb807f7bd22d9d8b34dffb3c3990898abca
amarDgb <amarhenni887@gmail.com>
2021-03-04 03:50:00 +0100

    Ajout de la base dune liste chaîner pour stocker les différentes valeurs-caractèristiques des processus à affichier

--- main.c
+++ main.c
@@ -5,6 +5,19 @@
 #include <sys/types.h>
 #include <string.h>  //strlen - strstr
 
+
+typedef struct Element Element;
+struct Element{
+  char elem[10];
+  Element *suivant;
+};
+
+typedef struct Liste Liste;
+struct Liste{
+  Element *premier;
+}; //cette structure contient un pointeur vers le premier element de la liste
+//pour trouver les element à venir il suffit d'utiliser le pointeur suivant
+
 int main()
 {
 
