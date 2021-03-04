95e6a6d4fc8c28e1cf8bcb0ef4b2c042690e5ffd
amarDgb <amarhenni887@gmail.com>
2021-03-04 03:52:19 +0100

    Ajout de la fonction Initialisation de la Liste chaîner

--- main.c
+++ main.c
@@ -18,6 +18,22 @@ struct Liste{
 }; //cette structure contient un pointeur vers le premier element de la liste
 //pour trouver les element à venir il suffit d'utiliser le pointeur suivant
 
+// 1- Initialisation de la Liste
+Liste *initialisation(){
+  Liste *liste = malloc(sizeof(liste)); //allocation de la structure de contrôle
+                                        //taille à allouée est calculer automatiquement avec sizeof
+  Element *element = malloc(sizeof(*element));//idem allocation de la mémoire pour le 1er Element
+  if(liste == NULL || element == NULL){//On vérifie si les allocations dynamiques en fonctionner
+    exit(EXIT_FAILURE); //En cas d'erreur en arrête le programme en faisant appel à exit
+  }
+  //Si tout c'est bien passer on définit les valeurs de notre premier élément
+  strcat(element-> elem, "VmSize");//valeur du premier pointeur
+  element->suivant = NULL;//le premier element est aussi le dernier pour le moment
+  liste->premier = element;
+
+  return liste;
+};
+
 int main()
 {
 
